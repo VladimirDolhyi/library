80de5413f3ce195388ea68e707cb0caba86f1469
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2025-01-12 08:40:21 +0200

    add admin, models & migrations for payments

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from borrowings.models import Borrowing
+
+
+class Payment(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "PENDING", "Pending"
+        PAID = "PAID", "Paid"
+
+    class Type(models.TextChoices):
+        PAYMENT = "PAYMENT", "Payment"
+        FINE = "FINE", "Fine"
+
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
+    type = models.CharField(max_length=20, choices=Type.choices)
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.SET_NULL, null=True, blank=True)
+    session_url = models.URLField()
+    session_id = models.CharField(max_length=255)
+    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.type} - {self.status} - {self.money_to_pay}"
