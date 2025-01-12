3f4db56e08659b90f90b820e853017527f1faa65
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2025-01-12 09:08:24 +0200

    update payments models

--- payments/models.py
+++ payments/models.py
@@ -8,13 +8,19 @@ class Payment(models.Model):
         PENDING = "PENDING", "Pending"
         PAID = "PAID", "Paid"
 
-    class Type(models.TextChoices):
+    class TypeField(models.TextChoices):
         PAYMENT = "PAYMENT", "Payment"
         FINE = "FINE", "Fine"
 
-    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
-    type = models.CharField(max_length=20, choices=Type.choices)
-    borrowing = models.ForeignKey(Borrowing, on_delete=models.SET_NULL, null=True, blank=True)
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING
+    )
+    type_field = models.CharField(max_length=20, choices=TypeField.choices)
+    borrowing = models.ForeignKey(
+        Borrowing, on_delete=models.SET_NULL, null=True, blank=True
+    )
     session_url = models.URLField()
     session_id = models.CharField(max_length=255)
     money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
