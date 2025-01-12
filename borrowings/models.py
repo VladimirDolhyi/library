c25342d97e8a166ffb03d9b4fd140b113e1b116c
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2025-01-12 07:57:33 +0200

    add is_returned for borrowings/models & migrations

--- borrowings/models.py
+++ borrowings/models.py
@@ -9,13 +9,17 @@ class Borrowing(models.Model):
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey(
-        Book, on_delete=models.CASCADE, related_name="borrowings"
+        Book, on_delete=models.CASCADE, related_name="borrowing"
     )
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name="borrowings"
+        related_name="borrowing"
     )
+
+    @property
+    def is_returned(self):
+        return self.actual_return_date is not None
 
     def __str__(self):
         return f"{self.user} borrowed {self.book.title}"
