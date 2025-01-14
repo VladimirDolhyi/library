1e98f881c9bf868bf63a0a124872489786a9440b
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2025-01-14 17:51:11 +0200

    add .env & .env.sample

--- library_service/settings.py
+++ library_service/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^jz7wrhry*ai5stl4+6bad4w$x7h0i6$5+8u*xm+u-9t05*4*&"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
